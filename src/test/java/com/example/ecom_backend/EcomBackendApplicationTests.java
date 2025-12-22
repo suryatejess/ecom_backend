e91f80cb04527044902f75bb90f56f7b62dac53c
Surya Tejess <suryatejessk@gmail.com>
2025-12-22 15:39:28 +0530

    initial commit

---
+++ src/test/java/com/example/ecom_backend/EcomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecom_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
